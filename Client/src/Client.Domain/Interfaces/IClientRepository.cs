8e76f9a88cf82c406c1dba45d289fedb075668e9
Andres Villavicencio <avillavicencio@devsu.com>
2024-12-07 11:09:50 -0500

    feat: Create ClientRepository interface

---
+++ Client/src/Client.Domain/Interfaces/IClientRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Client.Domain.Interfaces;
+
+public interface IClientRepository
+{
+    Task<IEnumerable<Entities.Client>> GetAllAsync();
+    Task<Entities.Client> GetByIdAsync(int id);
+    Task<Entities.Client> AddAsync(Entities.Client client);
+    Task UpdateAsync(Entities.Client client);
+    Task DeleteAsync(int id);
+}
