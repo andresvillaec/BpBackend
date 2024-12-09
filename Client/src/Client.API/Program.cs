a104d42439efd12702e27985af2b33c02b1b6d67
Andres Villavicencio <avillavicencio@devsu.com>
2024-12-08 22:32:31 -0500

    feat: Create migration on client microservice

--- Client/src/Client.API/Program.cs
+++ Client/src/Client.API/Program.cs
@@ -1,11 +1,25 @@
+using Client.Infrastructure;
+using Client.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Add application and infrastructure services
+builder.Services.AddInfrastructure(builder.Configuration);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ClientContext>();
+    dbContext.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
---
+++ Client/src/Client.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Client.Domain.Interfaces;
+using Client.Infrastructure.Persistence;
+using Client.Infrastructure.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Client.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ClientContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly(typeof(ClientContext).Assembly.FullName)
+                ));
+
+            services.AddScoped<IClientRepository, ClientRepository>();
+
+            return services;
+        }
+    }
+}
