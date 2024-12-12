3422bad0bbac085784af8ab364bee08c522c389d
Andres Villavicencio <avillavicencio@devsu.com>
2024-12-12 16:32:25 -0500

    feat: Implement movement endpoint

---
+++ Account/src/Account.API/Endpoints/MovementEndpoints.cs
@@ -0,0 +1,18 @@
+﻿using Carter;
+
+namespace Account.API.Endpoints
+{
+    public class MovementEndpoints : CarterModule
+    {
+        public MovementEndpoints()
+            :base("/api/movements")
+        {
+            
+        }
+
+        public override void AddRoutes(IEndpointRouteBuilder app)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
